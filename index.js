5e7f109a8d4ba77ec0344438427ec3d0cf668706
Aditya Rajput <adityarajput81971@gmail.com>
2025-11-26 20:53:38 +0530

    cors env

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CORS_ORIGIN,
+        origin: [process.env.CORS_ORIGIN , "http://localhost:5173"],
         methods: ["GET", "POST"],
         credentials: true
     }
